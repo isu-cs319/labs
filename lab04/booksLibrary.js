3d50a12dbabce03649a309f95bc27a1c73e65ceb
DavidSchott <daschott@iastate.edu>
2016-10-09 02:06:54 -0500

    fixed some bugs with populating shelfs with books

--- lab04/booksLibrary.js
+++ lab04/booksLibrary.js
@@ -1,6 +1,40 @@
 /**
  * Created by schott on 08.10.16.
  */
+// Build library
+var titles =   [
+    "exotic",
+    "dispensable",
+    "ossified",
+    "cold",
+    "seashore",
+    "needle",
+    "language",
+    "polish",
+    "sable",
+    "steel",
+    "thaw",
+    "object",
+    "bewildered",
+    "pine",
+    "quarrelsome",
+    "careful",
+    "eye",
+    "reduce",
+    "territory",
+    "knot",
+    "fly",
+    "unadvised",
+    "whistle",
+    "man",
+    "expansion",
+    "itch"];
+
+var maxShelfSize;
+var lib = constructLibrary();
+displayLibrary(lib);
+
+
 function Book(id, name, reference) {
     this.bookID = id;
     this.bookName = name;
@@ -18,56 +52,31 @@ function Library(books) {
     this.science = new Shelf("Science");
     this.sport = new Shelf("Sport");
     this.literature = new Shelf("Literature");
-    books.forEach(function(book){
-        if (book.bookID % 4 == 0){
-            this.art.books.push(book);
+    var length = books.length;
+    var i;
+    for (i = 0; i < length; i++){
+        if (books[i].bookID % 4 == 0){
+            this.art.books.push(books[i]);
         }
-        else if (book.bookID % 4 == 1){
-            this.science.books.push(book);
+        else if (books[i].bookID % 4 == 1){
+            this.science.books.push(books[i]);
         }
-        else if (book.bookID % 4 == 2){
-            this.sport.books.push(book);
+        else if (books[i].bookID % 4 == 2){
+            this.sport.books.push(books[i]);
         }
         else{
-            this.literature.books.push(book);
+            this.literature.books.push(books[i]);
         }
-    });
+    }
+    maxShelfSize = Math.max(this.art.books.length,this.science.books.length,this.sport.books.length,this.literature.books.length);
 }
 
-
-var titles =   [
-"exotic",
-"dispensable",
-"ossified",
-"cold",
-"seashore",
-"needle",
-"language",
-"polish",
-"sable",
-"steel",
-"thaw",
-"object",
-"bewildered",
-"pine",
-"quarrelsome",
-"careful",
-"eye",
-"reduce",
-"territory",
-"knot",
-"fly",
-"unadvised",
-"whistle",
-"man",
-"expansion",
-"itch"];
-
+function constructLibrary(){
 var i;
 var title;
 var books = [];
 for (i=0; i<25; i++){
-    title = titles[Math.floor(Math.random()*titles.length)];
+    title = titles[Math.floor(Math.random()*titles.length)] + " "+titles[Math.floor(Math.random()*titles.length)];
     if (i < 5){
         books.push(new Book(i,title,true));
     }
@@ -76,5 +85,36 @@ for (i=0; i<25; i++){
     }
     console.log(books[i]);
 }
-var lib = new Library(books);
-console.log(lib);
+return new Library(books);
+}
+
+function displayLibrary(lib){
+    populateShelf(lib.literature,0);
+    populateShelf(lib.art,1);
+    populateShelf(lib.science,2);
+    populateShelf(lib.sport,3);
+}
+
+function populateShelf(shelf, columnIndex){
+    var tbody = document.getElementById("library");
+    var tbook;
+    var numRows = tbody.rows.length;
+    var trow;
+    var tcell;
+    // Take care of <tr>s
+    var i = 0;
+    if (maxShelfSize != numRows){
+    for (i=0; i< maxShelfSize; i++){
+        trow = tbody.insertRow(i);
+        tcell = trow.insertCell(columnIndex);
+        tcell.innerHTML = shelf.books.bookName; // Make this more elaborate
+        }
+    }
+    else{
+    for(i=0; i <numRows; i++){
+        trow = tbody.rows[i];
+        tcell = trow.insertCell(columnIndex);
+        tcell.innerHTML = shelf.books.bookName; // Make this more elaborate
+    }
+    }
+}
