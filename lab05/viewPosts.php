ca6d2c71e178ed3ec1782ab73b2d926b8e04aeb9
DavidSchott <daschott@iastate.edu>
2016-10-17 15:32:22 -0500

    Now displaying table from posts.json

--- lab05/viewPosts.php
+++ lab05/viewPosts.php
@@ -2,8 +2,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Contact Information</title>
+    <title>Viewing Posts</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <style>
+        table, th, td {
+            border: 1px solid black;
+            padding: 5px;
+        }
+    </style>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <script>
@@ -25,14 +31,15 @@
     </thead>
     <tbody>
     <?php
-    $posts_dump = json_decode(file_get_contents("posts.json"));
+    $posts_dump = json_decode(file_get_contents("posts.json"), true);
     foreach ($posts_dump as $post){
         echo '<tr>';
-        echo '<td>' . $post->{"title"} . '</td>';
-        echo '<td>' . $post->{"body"} . '</td>';
-        echo '<td>' . $post->{"time"} . '</td>';
-        echo '<td>' . $post->{"user"} . '</td>';
-        echo '<td><button type="button" value="' . $post->{"id"} . '" onclick="editPost(this.value)"> Edit Post ' . $post->{"id"} . '</button></td>';
+        echo '<td>' . $post["title"] . '</td>';
+        echo '<td>' . $post["body"] . '</td>';
+        echo '<td>' . $post["time"] . '</td>';
+        echo '<td>' . $post["user"] . '</td>';
+        echo '<td><button type="button" value="' . $post["id"] . '" onclick="editPost(this.value)"> Edit Post ' . $post["id"] . '</button></td>';
+        echo '</tr>';
     }
     ?>
     </tbody>
