f14b175bfa4a5e3d8e41a7f572284b4558f7d359
eaanders <eaanders@iastate.edu>
2016-10-25 13:52:13 -0500

    a start to signing up & logging in (not done)

---
+++ lab06/login.php
@@ -0,0 +1,11 @@
+<?php
+
+session_start();
+
+if (isset($_POST["username"])) {
+$_SESSION["username"] = $_POST["username"];
+}
+
+header("Location: manage.php");
+
+?>
--- lab06/manage.php
+++ lab06/manage.php
@@ -5,6 +5,26 @@ require_once 'library.php';
 require_once 'shelf.php';
 // Start the session
 session_start();
-$_SESSION["username"] = "david";
+?>
+
+<!DOCTYPE html>
+
+<head>
+<style>
+div {
+    text-align: right;
+}
+</style>
+</head>
+
+<body>
+<div> Hello, <?php echo $_SESSION["username"]; ?>. <a href="logout.php">Click to logout</a> </div>
+<br>
+  
+</body>
+</html>
+
+<?php
 $std = new student($_SESSION["username"]);
 $std->addBook(0,"Harry Potter","J.K. Rowling");
+?>
